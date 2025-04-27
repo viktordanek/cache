730be233edf8f1191425865df24992925f86061b
Viktor Danek <viktordanek10@gmail.com>
2025-04-27 15:56:26 -0400

    upstream fix error

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
             environment-variable.url = "github:/viktordanek/environment-variable" ;
             flake-utils.url = "github:numtide/flake-utils" ;
             nixpkgs.url = "github:NixOs/nixpkgs" ;
-            shell-script.url = "github:viktordanek/shell-script/scratch/73ef5480-0376-4935-8a25-e03935e45b6c" ;
+            shell-script.url = "github:viktordanek/shell-script/scratch/a023ecbf-00f7-466a-9a97-9d83e1b1fc4c" ;
             visitor.url = "github:viktordanek/visitor/scratch/1bd1c881-b72b-43d7-a819-f6072a9dfdf7" ;
         } ;
     outputs =
