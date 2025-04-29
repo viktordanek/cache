9b8cb3a842250349bf2151150c490cb3648380c3
Viktor Danek <viktordanek10@gmail.com>
2025-04-29 06:41:31 -0400

    cache/test-and-commit add standard input, args, and seed to hash

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ then
     export ${TIMESTAMP_ENVIRONMENT_VARIABLE}
 fi &&
   PARENT_HASH=${!HASH_ENVIRONMENT_VARIABLE} &&
-  declare "${HASH_ENVIRONMENT_VARIABLE}"=$( ${ECHO} ${PRE_HASH} $(( ${!TIMESTAMP_ENVIRONMENT_VARIABLE} / ${LIFESPAN} )) | ${SHA512SUM} | ${CUT} --bytes -128 )
+  declare "${HASH_ENVIRONMENT_VARIABLE}"=$( ${ECHO} '${PRE_HASH} $(( ${!TIMESTAMP_ENVIRONMENT_VARIABLE} / ${LIFESPAN} )) ${HAS_STANDARD_INPUT} ${STANDARD_INPUT} ${@}' | ${SHA512SUM} | ${CUT} --bytes -128 )
   if [ ! -d ${RESOURCES}/${!HASH_ENVIRONMENT_VARIABLE} ]
   then
     ${MKDIR} ${RESOURCES}/${!HASH_ENVIRONMENT_VARIABLE} &&
@@ -18,55 +18,55 @@ fi &&
 
 
 
-    export RESOURCE=${RESOURCES}/${!HASH_ENVIRONMENT_VARIABLE} &&
-    export RESOURCE_NAME=${!HASH_ENVIRONMENT_VARIABLE} &&
-    export TARGET_MOUNT=${RESOURCE}/mount &&
-    export TARGET=${TARGET_MOUNT}/target &&
-    ${MKDIR} ${TARGET_MOUNT} &&
-  #
-    if ${HAS_STANDARD_INPUT}
-    then
-      if ${ECHO} "${STANDARD_INPUT}" | ${INIT} ${@} > ${RESOURCE}/init.standard-output 2> ${RESOURCE}/init.standard-error
+      export RESOURCE=${RESOURCES}/${!HASH_ENVIRONMENT_VARIABLE} &&
+      export RESOURCE_NAME=${!HASH_ENVIRONMENT_VARIABLE} &&
+      export TARGET_MOUNT=${RESOURCE}/mount &&
+      export TARGET=${TARGET_MOUNT}/target &&
+      ${MKDIR} ${TARGET_MOUNT} &&
+#
+      if ${HAS_STANDARD_INPUT}
       then
-        STATUS=${?}
+        if ${ECHO} "${STANDARD_INPUT}" | ${INIT} ${@} > ${RESOURCE}/init.standard-output 2> ${RESOURCE}/init.standard-error
+        then
+          STATUS=${?}
+        else
+          STATUS=${?}
+        fi
       else
-        STATUS=${?}
-      fi
-    else
-      if ${INIT} ${@} > ${RESOURCE}/init.standard-output 2> ${RESOURCE}/init.standard-error
+        if ${INIT} ${@} > ${RESOURCE}/init.standard-output 2> ${RESOURCE}/init.standard-error
+        then
+          STATUS=${?}
+        else
+          STATUS=${?}
+        fi
+      fi &&
+      ${ECHO} ${?} > ${RESOURCE}/init.status &&
+#
+      source ${MAKE_WRAPPER}/nix-support/setup-hook &&
+#
+      makeWrapper ${MAKE_WRAPPER_TEARDOWN} ${RESOURCE}/teardown.sh --set ORIGINATOR_PID ${ORIGINATOR_PID} --set RESOURCE_NAME ${RESOURCE_NAME} --set RESOURCES ${RESOURCES} --set STATUS 0 &&
+#
+      makeWrapper ${MAKE_WRAPPER_TEARDOWN} ${RESOURCE}/teardown.sh --set ORIGINATOR_PID ${ORIGINATOR_PID} --set RESOURCE_NAME ${RESOURCE_NAME} --set RESOURCES ${RESOURCES} --set STATUS ${STATUS} &&
+#
+#
+      ( ${RESOURCE}/teardown.sh > /dev/null 2>&1 & ) && ## KLUDGE ALERT:  We should not have to redirect standard output and error.  this probably indicates an error. FIXME UNCOMMENT ME
+#
+#
+      if [ ${STATUS} != 0 ]
       then
-        STATUS=${?}
+        exit ${INITIALIZATION_ERROR_CODE}
+      elif [ $( ${FIND} ${TARGET_MOUNT} -mindepth 1 -maxdepth 1 | ${WC} --lines ) -gt 1 ]
+      then
+        exit ${OVER_INITIALIZED_TARGET_ERROR_CODE}
+      elif [ ! -z "$( ${CAT} ${RESOURCE}/init.standard-error )" ]
+      then
+        exit ${STDERR_EMITTED_ERROR_CODE}
       else
-        STATUS=${?}
+        ${ECHO} ${TARGET}
       fi
-    fi &&
-    ${ECHO} ${?} > ${RESOURCE}/init.status &&
-  #
-    source ${MAKE_WRAPPER}/nix-support/setup-hook &&
-  #
-    makeWrapper ${MAKE_WRAPPER_TEARDOWN} ${RESOURCE}/teardown.sh --set ORIGINATOR_PID ${ORIGINATOR_PID} --set RESOURCE_NAME ${RESOURCE_NAME} --set RESOURCES ${RESOURCES} --set STATUS 0 &&
-  #
-    makeWrapper ${MAKE_WRAPPER_TEARDOWN} ${RESOURCE}/teardown.sh --set ORIGINATOR_PID ${ORIGINATOR_PID} --set RESOURCE_NAME ${RESOURCE_NAME} --set RESOURCES ${RESOURCES} --set STATUS ${STATUS} &&
-  #
-  #
-    ( ${RESOURCE}/teardown.sh > /dev/null 2>&1 & ) && ## KLUDGE ALERT:  We should not have to redirect standard output and error.  this probably indicates an error. FIXME UNCOMMENT ME
-  #
-  #
-    if [ ${STATUS} != 0 ]
-    then
-      exit ${INITIALIZATION_ERROR_CODE}
-    elif [ $( ${FIND} ${TARGET_MOUNT} -mindepth 1 -maxdepth 1 | ${WC} --lines ) -gt 1 ]
-    then
-      exit ${OVER_INITIALIZED_TARGET_ERROR_CODE}
-    elif [ ! -z "$( ${CAT} ${RESOURCE}/init.standard-error )" ]
-    then
-      exit ${STDERR_EMITTED_ERROR_CODE}
-    else
+#
       ${ECHO} ${TARGET}
-    fi
-  #
-    ${ECHO} ${TARGET}
-  #
+#
   else
     ${ECHO} ${HASH}
   fi
