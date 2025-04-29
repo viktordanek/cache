1337295ed5de25cf44a0d18b6022096e2227f0bb
Viktor Danek <viktordanek10@gmail.com>
2025-04-29 17:02:28 -0400

    WTF

--- vacuum.sh
+++ vacuum.sh
@@ -17,7 +17,7 @@ INDEX=$( ${FIND} /archive -mindepth 1 -maxdepth 1 -type d | ${WC} --lines ) &&
         # BELOW IS A KLUDGE.
         # IT WOULD BE BETTER TO REMOVE THE PID LINES
         # THIS IS BECAUSE WE ARE VACUMMING THE PIDs, PARENT_HASH, and FLAGs
-        ${RM} /archive/${INDEX}/release.standard-output
+        ${RM} --force /archive/${INDEX}/release.standard-output
   fi &&
   ${RM} --recursive --force /archive/${INDEX}/*.pid &&
   ${RM} --recursive --force /archive/${INDEX}/*.hash &&
