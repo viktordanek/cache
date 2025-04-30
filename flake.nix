e103b2751f4f90764d1ecd79d44b848000e20533
Viktor Danek <viktordanek10@gmail.com>
2025-04-30 14:52:02 -0400

    cache/test-and-commit wait for process wtf

--- flake.nix
+++ flake.nix
@@ -688,18 +688,40 @@
                                                                             [ 13 ]
                                                                             [ 14 ]
                                                                             [ 15 ]
-                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 38 ] )
-                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 39 ] )
-                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 40 ] )
-                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 41 ] )
-                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 42 ] )
-                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 43 ] )
-                                                                            ( if builtins.typeOf post == "null" then [ ] else [ 46 ] )
-                                                                            [ 48 ]
-                                                                            [ 49 ]
-                                                                            [ 50 ]
-                                                                            [ 51 ]
-                                                                            [ 52 ]
+                                                                            [ 16 ]
+                                                                            [ 17 ]
+                                                                            [ 18 ]
+                                                                            [ 19 ]
+                                                                            [ 20 ]
+                                                                            [ 21 ]
+                                                                            [ 22 ]
+                                                                            [ 23 ]
+                                                                            [ 24 ]
+                                                                            [ 25 ]
+                                                                            [ 26 ]
+                                                                            [ 27 ]
+                                                                            [ 28 ]
+                                                                            [ 29 ]
+                                                                            [ 30 ]
+                                                                            [ 31 ]
+                                                                            [ 32 ]
+                                                                            [ 33 ]
+                                                                            [ 34 ]
+                                                                            [ 35 ]
+                                                                            [ 36 ]
+                                                                            [ 37 ]
+                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 68 ] )
+                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 69 ] )
+                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 70 ] )
+                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 71 ] )
+                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 72 ] )
+                                                                            ( if builtins.typeOf primary.release == "null" then [ ] else [ 73 ] )
+                                                                            ( if builtins.typeOf post == "null" then [ ] else [ 76 ] )
+                                                                            [ 78 ]
+                                                                            [ 79 ]
+                                                                            [ 80 ]
+                                                                            [ 81 ]
+                                                                            [ 82 ]
                                                                         ] ;
                                                                 with-index = builtins.genList ( index : { index = index ; line = builtins.elemAt all index ; } ) ( builtins.length all ) ;
                                                                 filtered = builtins.filter ( x : builtins.any ( i : x.index == i ) array ) with-index ;
--- teardown.sh
+++ teardown.sh
@@ -1,5 +1,31 @@
 TIMEOUT=$(( ${LIFESPAN} - ( $( ${DATE} +%s ) % ${LIFESPAN} ) )) &&
   ( ${INOTIFYWAIT} --event delete /mount/${RESOURCE_NAME}/TEARDOWN_START_FLAG --timeout ${TIMEOUT} --quiet || ${TRUE} ) &&
+  ${FIND} /mount/${RESOURCE_NAME} -mindepth 1 -maxdepth 1 -type l -name "*.hash" | while read HASH_LINK
+  do
+    DIRECTORY=$( ${READLINK} ${HASH_LINK} ) &&
+    exec 202> ${DIRECTORY}.lock &&
+    if ${FLOCK} 202
+    then
+      if [ -d ${DIRECTORY} ]
+      then
+        if [ -f ${DIRECTORY}/TEARDOWN_START_FLAG ]
+        then
+          ${RM} ${DIRECTORY}/TEARDOWN_START_FLAG &&
+            ${FLOCK} -u 202 &&
+            ${INOTIFYWAIT} --event delete ${DIRECTORY} --quiet &&
+            ${RM} ${HASH_LINK}
+        else
+          ${FLOCK} -u 202
+            ${INOTIFYWAIT} --event delete ${DIRECTORY} --quiet &&
+            ${RM} ${HASH_LINK}
+        fi
+      else
+        ${RM} ${HASH_LINK}
+      fi
+    else
+      exit ${LOCK_FAILURE}
+    fi
+  done &&
   ${FIND} /mount/${RESOURCE_NAME} -mindepth 1 -maxdepth 1 -type f -name "*.pid" | while read PID_FILE
   do
     PID=$( ${CAT} ${PID_FILE} ) &&
@@ -10,6 +36,10 @@ TIMEOUT=$(( ${LIFESPAN} - ( $( ${DATE} +%s ) % ${LIFESPAN} ) )) &&
   if ${FLOCK} 201
   then
     export RESOURCE=/mount/${RESOURCE_NAME} &&
+
+
+
+
 
 
 
