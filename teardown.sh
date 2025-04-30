9a968ef8deba8242efeba6db56f590876a754d82
Viktor Danek <viktordanek10@gmail.com>
2025-04-30 11:39:48 -0400

    I am not sure why this is not working

--- teardown.sh
+++ teardown.sh
@@ -1,10 +1,10 @@
-exec 201> /mount/${RESOURCE_NAME}/lock &&
+exec 201> /mount/${RESOURCE_NAME}.lock &&
   if ${FLOCK} 201
   then
    if [ ${STATUS} == 0 ]
     then
-      TIMEOUT=$(( ${LIFESPAN} - ( $( ${DATE} +%s ) % ${LIFESPAN} ) )) &&
-        ${ECHO} ${INOTIFYWAIT} --event delete /mount/${RESOURCE_NAME}/TEARDOWN_START_FLAG --timeout ${TIMEOUT} --quiet > /dev/null 2>&1 &&
+      TIMEOUT=$(( ${LIFESPAN} - ( $( ${DATE} +%s ) % ${LIFESPAN} ) )) && touch /mount && touch /mount/${RESOURCE_NAME} && touch / && touch /mount/${RESOURCE_NAME}/lock && touch /mount/${RESOURCE_NAME}/lock &&
+        ${INOTIFYWAIT} --event delete /mount/${RESOURCE_NAME}/TEARDOWN_START_FLAG --timeout ${TIMEOUT} --quiet &&
         ${TAIL} --follow /dev/null --pid ${ORIGINATOR_PID}
     fi &&
     export RESOURCE=/mount/${RESOURCE_NAME} &&
@@ -46,7 +46,7 @@ exec 201> /mount/${RESOURCE_NAME}/lock &&
 #
        ( ${POST} || ${TRUE} ) &&
 #
-      ${RM} --recursive --force /mount/${RESOURCE_NAME}
+      ${RM} --recursive --force /mount/${RESOURCE_NAME}.lock /mount/${RESOURCE_NAME}
   else
     ${ECHO} FAILED TO LOCK /mount/${RESOURCE_NAME}/lock >&2 &&
       exit ${LOCK_FAILURE}
